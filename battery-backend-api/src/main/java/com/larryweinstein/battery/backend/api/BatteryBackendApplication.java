01ac0bae2c8dab4b585ef55065f3fc68a0d0aea6
Lawrence Weinstein <larryweinstein42@gmail.com>
2024-06-16 14:13:33 -0700

    Added in memory verification

--- battery-backend-api/src/main/java/com/larryweinstein/battery/backend/api/BatteryBackendApplication.java
+++ battery-backend-api/src/main/java/com/larryweinstein/battery/backend/api/BatteryBackendApplication.java
@@ -1,11 +1,20 @@
 package com.larryweinstein.battery.backend.api;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication(scanBasePackages = {"com.larryweinstein.battery.backend"})
 public class BatteryBackendApplication {
-
+    @Bean
+    UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(User.withDefaultPasswordEncoder()
+                .username("trogdor").password("burninator").roles("USER").build());
+    }
     public static void main(String[] args) {
         SpringApplication.run(BatteryBackendApplication.class, args);
     }
