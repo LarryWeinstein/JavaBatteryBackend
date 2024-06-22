6e2c0d59a0cbf15048b6b4ef2ac4c405c520c130
Lawrence Weinstein <larryweinstein42@gmail.com>
2024-06-22 07:36:25 -0700

    Created migration to create user and role tables to support authentication

---
+++ battery-backend-flyway/migrations/V0.0.1_002__Battery_Backend_Model_002.sql
@@ -0,0 +1,37 @@
+-- V1__Create_Battery_and_ProcessedDataLine_Tables.sql
+
+-- Create sequence for user table
+CREATE SEQUENCE seq_user
+    start with 1_000
+    minvalue 1_000
+    increment by 1
+    no maxvalue
+    cache 1;
+
+CREATE SEQUENCE seq_role
+    start with 1_000
+    minvalue 1_000
+    increment by 1
+    no maxvalue
+    cache 1;
+
+-- Create the user table
+CREATE TABLE "user" (
+    id INT DEFAULT NEXTVAL('seq_user') PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,    
+    password VARCHAR(30) NOT NULL   
+);
+
+-- Create the user table
+CREATE TABLE role (
+    id INT DEFAULT NEXTVAL('seq_role') PRIMARY KEY,
+    name VARCHAR(20)  
+);
+
+CREATE TABLE user_role (
+    user_id BIGINT NOT NULL CONSTRAINT fk_r_user_role_user references "user"(id),
+    role_id BIGINT NOT NULL CONSTRAINT fk_r_user_role_role references role(id),
+    CONSTRAINT pk_r_user_roel PRIMARY KEY (user_id, role_id)
+);
+
