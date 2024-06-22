c5a79bcb191ded713a9cf6003754eabcefa0acf7
Lawrence Weinstein <larryweinstein42@gmail.com>
2024-06-22 08:25:39 -0700

    Added repository annotation to UserRepository

--- battery-backend-repository/src/main/java/com/larryweinstein/battery/backend/repository/UserRepository.java
+++ battery-backend-repository/src/main/java/com/larryweinstein/battery/backend/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.larryweinstein.battery.backend.repository;
 
 import com.larryweinstein.battery.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByusername(String username);
     Boolean existsByusername(String username);
