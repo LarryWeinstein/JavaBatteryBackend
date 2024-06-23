72c0557c39708d2822686c81cda207e65b19cca2
Lawrence Weinstein <larryweinstein42@gmail.com>
2024-06-23 12:04:27 -0700

    Removed Roles for now

--- battery-backend-model/src/main/java/com/larryweinstein/battery/backend/model/User.java
+++ battery-backend-model/src/main/java/com/larryweinstein/battery/backend/model/User.java
@@ -45,11 +45,12 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-
+/*
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles = new HashSet<>();*/
 
+    /*
     @Override
     public String toString() {
         return "User{" +
@@ -58,6 +59,16 @@ public class User implements UserDetails {
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", roles=" + roles +
+                '}';
+    }*/
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 
